aef9c98be980faea68895c59d952c7f1b876055c
alensh <alensh.am12@gmail.com>
2020-08-18 15:54:59 +0545

    changes in plugin

--- android/src/main/kotlin/com/contedevel/accountmanager/AccountManagerPlugin.kt
+++ android/src/main/kotlin/com/contedevel/accountmanager/AccountManagerPlugin.kt
@@ -2,12 +2,16 @@ package com.contedevel.accountmanager
 
 import android.accounts.Account
 import android.accounts.AccountManager
+import android.accounts.AccountManagerCallback
 import android.app.Activity
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.text.TextUtils
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.core.app.ActivityCompat
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -45,8 +49,22 @@ class AccountManagerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, Pl
             val accountManager = AccountManager.get(it)
             val account = Account(accountName, packageName)
             val userData = Bundle()
-            userData.putStringArrayList("userData", arrayListOf(accountPlan, accountType))
+            userData.putString("account_plan", accountPlan);
+            userData.putString("account_type", accountType);
             val wasAdded = accountManager.addAccountExplicitly(account, token, userData)
+            accountManager.setAuthToken(account, AUTHTOKEN_TYPE_FULL_ACCESS, token);
+            accountManager.setUserData(account, "token_saved", token);
+            if (wasAdded) {
+                val bundle = Bundle();
+                val intent = Intent();
+                bundle.putString(AccountManager.KEY_AUTHTOKEN, token)
+                bundle.putString(AccountManager.KEY_ACCOUNT_NAME, accountName)
+                bundle.putString(AccountManager.KEY_ACCOUNT_TYPE, packageName)
+                intent.putExtras(bundle)
+                it.setResult(RESULT_OK, intent)
+            } else {
+                Toast.makeText(it.applicationContext, "ERROR", Toast.LENGTH_LONG).show()
+            }
             result.success(wasAdded)
         }
     }
@@ -56,13 +74,13 @@ class AccountManagerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, Pl
             val accounts = AccountManager.get(it).accounts
             val list = mutableListOf<HashMap<String, String>>()
             for (account in accounts) {
-                if(account!=null) {
+                if (account != null) {
                     list.add(hashMapOf(
                             ACCOUNT_NAME to account.name,
                             PACKAGE_NAME to account.type,
                             ACCOUNT_TOKEN to AccountManager.get(it).getPassword(account),
-                            ACCOUNT_PLAN to AccountManager.get(it).getUserData(account, "userData")[0].toString(),
-                            ACCOUNT_TYPE to AccountManager.get(it).getUserData(account, "userData")[1].toString()
+                            ACCOUNT_PLAN to AccountManager.get(it).getUserData(account, "account_plan"),
+                            ACCOUNT_TYPE to AccountManager.get(it).getUserData(account, "account_type")
 
                     ))
                 }
@@ -87,20 +105,63 @@ class AccountManagerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, Pl
 
     private fun removeAccount(call: MethodCall, result: Result) {
         activity?.let {
-            val accountName = call.argument<String>(ACCOUNT_NAME)
-            val accountType = call.argument<String>(ACCOUNT_TYPE)
+            val packageName = call.argument<String>(PACKAGE_NAME)
             val accountManager = AccountManager.get(it)
-            val account = Account(accountName, accountType)
-
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP_MR1) {
-                val future = accountManager.removeAccount(account, {}, Handler())
-                result.success(future.getResult(2, TimeUnit.SECONDS))
-            } else {
-                val wasRemoved = accountManager.removeAccountExplicitly(account)
-                result.success(wasRemoved)
+            val account = accountManager.getAccountsByType(packageName);
+            for (i in account.indices) {
+                accountManager.getAuthToken(account[i], AUTHTOKEN_TYPE_FULL_ACCESS, null, true, AccountManagerCallback { bundle ->
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP_MR1) {
+                        val future = accountManager.removeAccount(account[i], {}, Handler())
+                        result.success(future.getResult(2, TimeUnit.SECONDS))
+                    } else {
+                        val wasRemoved = accountManager.removeAccountExplicitly(account[i])
+                        result.success(wasRemoved)
+                    }
+                }, null)
             }
         }
-        result.success(false)
+    }
+
+    private fun invalidateAuthToken(call: MethodCall,result: Result) {
+        activity?.let {
+            val packageName = call.argument<String>(PACKAGE_NAME)
+            val accountManager = AccountManager.get(it)
+            val availableAccounts = accountManager.getAccountsByType(packageName)
+            if (availableAccounts.isEmpty()) {
+                result.success(false)
+            } else {
+                for (i in availableAccounts.indices) {
+                    accountManager.getAuthToken(availableAccounts[i], AUTHTOKEN_TYPE_FULL_ACCESS, null, true, AccountManagerCallback { bundle ->
+                        bundle.getResult(5L, TimeUnit.SECONDS);
+                        val authToken = bundle.result.getString(AccountManager.KEY_AUTHTOKEN);
+                        val accountType = bundle.result.getString(AccountManager.KEY_ACCOUNT_TYPE)
+                        accountManager.invalidateAuthToken(accountType, authToken)
+                        result.success(true);
+                    }, null)
+                }
+
+            }
+        }
+    }
+
+    private fun setAuthToken(call: MethodCall,result: Result) {
+        activity?.let {
+            val packageName = call.argument<String>(PACKAGE_NAME)
+            val refreshToken = call.argument<String>(ACCOUNT_REFRESH_TOKEN)
+            val accountManager = AccountManager.get(it)
+            val availableAccounts = accountManager.getAccountsByType(packageName)
+            if (availableAccounts.isEmpty()) {
+                result.success(false)
+            } else {
+                for (i in availableAccounts.indices) {
+                    accountManager.setAuthToken(availableAccounts[i], AUTHTOKEN_TYPE_FULL_ACCESS, refreshToken)
+                    accountManager.setUserData(availableAccounts[i],"token_saved",refreshToken)
+                    result.success(true)
+
+                }
+
+            }
+        }
     }
 
     override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
@@ -109,7 +170,39 @@ class AccountManagerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, Pl
             "getAccounts" -> getAccounts(result)
             "removeAccount" -> removeAccount(call, result)
             "peekAccounts" -> peekAccounts(result);
+            "invalidateAuthToken" -> invalidateAuthToken(call,result)
+            "getAuthToken" -> getAuthToken(call, result)
+            "setRefreshToken" -> setAuthToken(call,result)
             else -> result.notImplemented()
+        }
+    }
+
+    private fun getAuthToken(call: MethodCall, result: Result) {
+        activity?.let {
+            var authData: Any? = ""
+            val packageName = call.argument<String>(PACKAGE_NAME)
+            val accountManager = AccountManager.get(it)
+            val availableAccounts = accountManager.getAccountsByType(packageName)
+            if (availableAccounts.isEmpty()) {
+                result.success(null);
+            } else {
+                val name = arrayOfNulls<String>(availableAccounts.size)
+                for (i in availableAccounts.indices) {
+                    name[i] = availableAccounts[i].name
+                    accountManager.getAuthToken(availableAccounts[i], AUTHTOKEN_TYPE_FULL_ACCESS, null, it, { bundle ->
+                        val authToken = bundle.result.getString(AccountManager.KEY_AUTHTOKEN);
+                        authData = authToken
+                        Log.e("DATA", authData.toString() + " " + availableAccounts[0])
+                        if (TextUtils.isEmpty(authToken)) {
+                            result.success(null)
+                        } else {
+                            result.success(authData);
+                        }
+
+                    }, null)
+                }
+
+            }
         }
     }
 
@@ -163,6 +256,8 @@ class AccountManagerPlugin : FlutterPlugin, MethodCallHandler, ActivityAware, Pl
         private const val ACCOUNT_TOKEN = "account_token"
         private const val ACCOUNT_PLAN = "account_plan"
         private const val PACKAGE_NAME = "package_name"
+        private const val AUTHTOKEN_TYPE_FULL_ACCESS = "Full access"
+        private const val ACCOUNT_REFRESH_TOKEN = "account_refresh_token"
 
         @Suppress("UNUSED")
         @JvmStatic
